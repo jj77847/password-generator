062e18c1a3ada0e833d42eb1228023e929e8a160
jj77847 <81172135+jj77847@users.noreply.github.com>
2021-09-29 18:37:23 +0100

    layout changes

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,5 +1,18 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var numberChars = "0123456789";
+var upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerChars = "abcdefghijklmnopqrstuvwxyz";
+var allChars = numberChars + upperChars + lowerChars;
+var randPasswordArray = Array(passwordLength);
+randPasswordArray[0] = numberChars;
+randPasswordArray[1] = upperChars;
+randPasswordArray[2] = lowerChars;
+
+function shuffleArray(array) {
+  for (var i = array.length - 1; i > 0; i--) {
+  }
+
 
 // Write password to the #password input
 function writePassword() {
@@ -12,25 +25,5 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// Code example search google/
 
 function generatePassword(passwordLength) {
-  var numberChars = "0123456789";
-  var upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var lowerChars = "abcdefghijklmnopqrstuvwxyz";
-  var allChars = numberChars + upperChars + lowerChars;
-  var randPasswordArray = Array(passwordLength);
-  randPasswordArray[0] = numberChars;
-  randPasswordArray[1] = upperChars;
-  randPasswordArray[2] = lowerChars;
-  randPasswordArray = randPasswordArray.fill(allChars, 3);
-  return shuffleArray(randPasswordArray.map(function(x) { return x[Math.floor(Math.random() * x.length)] })).join('');
-}
-
-function shuffleArray(array) {
-  for (var i = array.length - 1; i > 0; i--) {
-    var j = Math.floor(Math.random() * (i + 1));
-    var temp = array[i];
-    array[i] = array[j];
-    array[j] = temp;
-  }
