a46b54b4787f76c486e8d14cdf649f77342bc258
jj77847 <81172135+jj77847@users.noreply.github.com>
2021-10-01 21:03:00 +0100

    edited comments js

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -4,8 +4,8 @@ var generateBtn = document.querySelector("#generate");
 function generatePassword() {
   prompt = "Please Choose a Password up to 8 characters and less than 120";
 
-  const confirm = lowercase "abcdefghijklmnopqrstuvwxyz"
-  const uppercase =  "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  const lowercase = "abcdefghijklmnopqrstuvwxyz"
+  const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
   const numeric = "1234567890"
   const specialCharacters = " !"#$%&'()*+,-./:;<=>?@[]^_`{|}~"
 
@@ -13,8 +13,6 @@ function generatePassword() {
   confirm;
   confirm;
 }
-
-const = 
 
 // Write password to the #password input
 function writePassword() {
@@ -27,16 +25,3 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// User Flow from notes.md
-
-// Click on generate password button
-// Presented with criteria
-// Fill in the criteria
-// Password should appear on the page
-// Steps
-
-// Declare a function generatePassword
-// Declare const for lowercase abcdefghijklmnopqrstuvwxyz
-// Declare const for uppercase ABCDEFGHIJKLMNOPQRSTUVWXYZ
-// Declare const for numeric 1234567890
-// Declare const for specialCharacters " !"#$%&'()*+,-./:;<=>?@[]^_`{|}~"
