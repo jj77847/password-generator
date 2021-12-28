13a29a20d0aef06c83d7ab066c6b240d84c79a80
jj77847 <81172135+jj77847@users.noreply.github.com>
2021-12-28 11:57:52 +0000

    layout and syntax errors

--- script.js
+++ script.js
@@ -1,17 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
 // Created Arrays of Possible Character Choices
 const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 const upperCase = [
@@ -158,3 +147,14 @@ function generatePassword() {
 
   return finalPassword;
 }
+
+// Write password to the #password
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
